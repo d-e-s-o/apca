7cfef29b0d27de4871f0fdb95265b97e3eb40314
Daniel Mueller <deso@posteo.net>
2019-04-21 17:29:09 -0700

    Add support for listing all supported assets
    This change adds support for listing all available assets. We inquire
    the required information by issuing a GET against the /v1/assets
    endpoint.

---
+++ src/api/v1/assets.rs
@@ -0,0 +1,85 @@
+// Copyright (C) 2019 Daniel Mueller <deso@posteo.net>
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+use serde::Serialize;
+
+use url::form_urlencoded::Serializer;
+
+use crate::api::v1::asset::Asset;
+use crate::api::v1::asset::Class;
+use crate::api::v1::asset::Status;
+use crate::requestor::Endpoint;
+use crate::Str;
+
+
+/// A GET request to be made to the /v1/assets endpoint.
+#[derive(Clone, Copy, Debug, Serialize, PartialEq)]
+pub struct AssetsReq {
+  /// The status of assets to include in the response.
+  #[serde(rename = "status")]
+  pub status: Status,
+  /// The asset class of which to include assets in the response.
+  #[serde(rename = "asset_class")]
+  pub class: Class,
+}
+
+
+/// The representation of a GET request to the /v1/assets endpoint.
+#[derive(Clone, Copy, Debug)]
+struct Get {}
+
+EndpointDef! {
+  Get,
+  Ok => Vec<Asset>, GetOk, [
+    /* 200 */ OK,
+  ],
+  Err => GetError, []
+}
+
+impl Endpoint for Get {
+  type Input = AssetsReq;
+  type Output = GetOk;
+  type Error = GetError;
+
+  fn path(_input: &Self::Input) -> Str {
+    "/v1/assets".into()
+  }
+
+  fn query(input: &Self::Input) -> Option<Str> {
+    let query = Serializer::new(String::new())
+      .append_pair("status", input.status.as_ref())
+      .append_pair("asset_class", input.class.as_ref())
+      .finish();
+
+    Some(query.into())
+  }
+}
+
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  use tokio::runtime::current_thread::block_on_all;
+
+  use crate::api::v1::asset::Exchange;
+  use crate::Error;
+  use crate::Requestor;
+
+
+  #[test]
+  fn list_assets() -> Result<(), Error> {
+    let reqtor = Requestor::from_env()?;
+    let request = AssetsReq {
+      status: Status::Active,
+      class: Class::UsEquity,
+    };
+    let future = reqtor.issue::<Get>(request)?;
+    let assets = block_on_all(future)?;
+    let asset = assets.iter().find(|x| x.symbol == "AAPL").unwrap();
+    assert_eq!(asset.class, Class::UsEquity);
+    assert_eq!(asset.exchange, Exchange::Nasdaq);
+    assert_eq!(asset.status, Status::Active);
+    Ok(())
+  }
+}
