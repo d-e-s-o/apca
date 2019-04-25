8205ec955cff4a768535249bd59530ac99846f44
Daniel Mueller <deso@posteo.net>
2019-04-24 18:38:57 -0700

    Add the "usual" lints to the crate
    The compiler can be tremendously helpful in catching issues early on and
    we should accept its help with open arms.
    To this end, this change denies a bunch of lints and warns for others.

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,53 @@
 // Copyright (C) 2019 Daniel Mueller <deso@posteo.net>
 // SPDX-License-Identifier: GPL-3.0-or-later
 
+#![deny(
+  dead_code,
+  illegal_floating_point_literal_pattern,
+  improper_ctypes,
+  intra_doc_link_resolution_failure,
+  late_bound_lifetime_arguments,
+  missing_copy_implementations,
+  missing_debug_implementations,
+  missing_docs,
+  no_mangle_generic_items,
+  non_shorthand_field_patterns,
+  overflowing_literals,
+  path_statements,
+  patterns_in_fns_without_body,
+  plugin_as_library,
+  private_in_public,
+  proc_macro_derive_resolution_fallback,
+  safe_packed_borrows,
+  stable_features,
+  trivial_bounds,
+  trivial_numeric_casts,
+  type_alias_bounds,
+  tyvar_behind_raw_pointer,
+  unconditional_recursion,
+  unions_with_drop_fields,
+  unreachable_code,
+  unreachable_patterns,
+  unstable_features,
+  unstable_name_collisions,
+  unused,
+  unused_comparisons,
+  unused_import_braces,
+  unused_lifetimes,
+  unused_qualifications,
+  unused_results,
+  where_clauses_object_safety,
+  while_true,
+)]
+#![warn(
+  bad_style,
+  future_incompatible,
+  nonstandard_style,
+  renamed_and_removed_lints,
+  rust_2018_compatibility,
+  rust_2018_idioms,
+)]
+
 //! A create for interacting with the Alpaca API.
 
 #[macro_use]
