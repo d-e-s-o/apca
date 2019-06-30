8252f5d202f6f2a096b4acef740c69f6a755a4e8
Daniel Mueller <deso@posteo.net>
2019-06-30 15:18:40 -0700

    Add support for listing open orders for v2
    This change adds support for retrieving the list of open orders through
    the /v2/orders endpoint. The request type itself is exactly the same as
    that for v1, it's only the resulting list of orders that now has the v2
    layout.

---
+++ src/api/v2/orders.rs
@@ -0,0 +1,69 @@
+// Copyright (C) 2019 Daniel Mueller <deso@posteo.net>
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+use url::form_urlencoded::Serializer;
+
+pub use crate::api::v1::orders::OrdersReq;
+
+use crate::api::v2::order::Order;
+use crate::endpoint::Endpoint;
+use crate::Str;
+
+
+/// The representation of a GET request to the /v2/orders endpoint.
+#[derive(Clone, Copy, Debug)]
+pub struct Get {}
+
+EndpointDef! {
+  Get,
+  Ok => Vec<Order>, [
+    /// The list of orders was retrieved successfully.
+    /* 200 */ OK,
+  ],
+  Err => GetError, []
+}
+
+impl Endpoint for Get {
+  type Input = OrdersReq;
+  type Output = Vec<Order>;
+  type Error = GetError;
+
+  fn path(_input: &Self::Input) -> Str {
+    "/v2/orders".into()
+  }
+
+  fn query(input: &Self::Input) -> Option<Str> {
+    let query = Serializer::new(String::new())
+      .append_pair("limit", &input.limit.to_string())
+      .finish();
+
+    Some(query.into())
+  }
+}
+
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  use test_env_log::test;
+
+  use tokio::runtime::current_thread::block_on_all;
+
+  use crate::api_info::ApiInfo;
+  use crate::Client;
+  use crate::Error;
+
+
+  #[test]
+  fn list_orders() -> Result<(), Error> {
+    let api_info = ApiInfo::from_env()?;
+    let client = Client::new(api_info)?;
+    let request = OrdersReq { limit: 50 };
+    let future = client.issue::<Get>(request)?;
+
+    // We merely check that no error is reported.
+    let _ = block_on_all(future)?;
+    Ok(())
+  }
+}
