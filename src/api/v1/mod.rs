f96ebf18c12402039e333b9527ea9c5ea1304734
Daniel Mueller <deso@posteo.net>
2019-12-23 12:50:41 -0800

    Move v1 event streaming functionality over to v2
    This change moves the v1 event streaming functionality over to v2 of the
    API.

--- src/api/v1/mod.rs
+++ src/api/v1/mod.rs
@@ -1,9 +1,6 @@
 // Copyright (C) 2019 Daniel Mueller <deso@posteo.net>
 // SPDX-License-Identifier: GPL-3.0-or-later
 
-/// Definitions for account and trade related events.
-pub mod events;
-
 // TODO: Make module private again once transition to v2 is done.
 #[cfg(test)]
 pub mod order_util;
