2a1de03699610048b74576416396fd3c4524c094
Daniel Mueller <deso@posteo.net>
2019-12-23 12:02:29 -0800

    Remove v1 account support
    This change marks the first step towards fully moving towards v2 of the
    Alpaca API. Upstream has deprecated the v1 API and we intend to remove
    support for it, as v2 is working as expected by now.
    To that end, this starts off the process by removing the v1::account
    module, integrating required functionality directly into v2::account.

--- src/api/v1/mod.rs
+++ src/api/v1/mod.rs
@@ -1,8 +1,6 @@
 // Copyright (C) 2019 Daniel Mueller <deso@posteo.net>
 // SPDX-License-Identifier: GPL-3.0-or-later
 
-/// Definitions pertaining the user's account.
-pub mod account;
 /// Definitions surrounding assets.
 pub mod asset;
 /// Functionality for listing available assets.
