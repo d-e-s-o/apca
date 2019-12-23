deb7361903f269d3308b792b51d2a4a4d3c61745
Daniel Mueller <deso@posteo.net>
2019-12-23 12:23:12 -0800

    Remove v1 orders support
    As a next step in migrating to v2 of the Alpaca API, this change removes
    the v1::orders module, integrating required functionality directly into
    v2::orders.

--- src/api/v2/orders.rs
+++ src/api/v2/orders.rs
@@ -1,13 +1,25 @@
 // Copyright (C) 2019 Daniel Mueller <deso@posteo.net>
 // SPDX-License-Identifier: GPL-3.0-or-later
 
-use url::form_urlencoded::Serializer;
+use serde::Serialize;
 
-pub use crate::api::v1::orders::OrdersReq;
+use url::form_urlencoded::Serializer;
 
 use crate::api::v2::order::Order;
 use crate::endpoint::Endpoint;
 use crate::Str;
+
+
+/// A GET request to be made to the /v2/orders endpoint.
+// Note that we do not expose or supply all parameters that the Alpaca
+// API supports.
+#[derive(Clone, Copy, Debug, Serialize, PartialEq)]
+pub struct OrdersReq {
+  /// The maximum number of orders in response. Defaults to 50 and max
+  /// is 500.
+  #[serde(rename = "limit")]
+  pub limit: u64,
+}
 
 
 /// The representation of a GET request to the /v2/orders endpoint.
@@ -48,6 +60,8 @@ mod tests {
 
   use test_env_log::test;
 
+  use crate::api::v1::order_util::order_aapl;
+  use crate::api::v2::order;
   use crate::api_info::ApiInfo;
   use crate::Client;
   use crate::Error;
@@ -59,8 +73,17 @@ mod tests {
     let client = Client::new(api_info);
     let request = OrdersReq { limit: 50 };
 
-    // We merely check that no error is reported.
-    let _ = client.issue::<Get>(request).await?;
+    let order = order_aapl(&client).await?;
+    let result = client.issue::<Get>(request.clone()).await;
+    let _ = client.issue::<order::Delete>(order.id).await?;
+    let before = result?;
+    let after = client.issue::<Get>(request.clone()).await?;
+
+    let before = Into::<Vec<_>>::into(before);
+    let after = Into::<Vec<_>>::into(after);
+
+    assert!(before.into_iter().find(|x| x.id == order.id).is_some());
+    assert!(after.into_iter().find(|x| x.id == order.id).is_none());
     Ok(())
   }
 }
