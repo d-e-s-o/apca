6dd95bee1fe25423f878d7404b392a372e869c29
Daniel Mueller <deso@posteo.net>
2022-03-04 09:32:14 -0800

    Introduce news API bindings
    TODO: Should be put behind 'unstable' feature or so.
    
    This change introduces bindings for the recently introduced news API.
    Note that this API is still in the Beta testing phase. As such, ...

--- src/data/mod.rs
+++ src/data/mod.rs
@@ -1,6 +1,8 @@
 // Copyright (C) 2020-2022 The apca Developers
 // SPDX-License-Identifier: GPL-3.0-or-later
 
+/// Definitions for the first version of the Alpaca Data API.
+pub mod v1;
 /// Definitions for the second version of the Alpaca Data API.
 pub mod v2;
 
