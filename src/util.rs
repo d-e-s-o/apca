87ec8067e76c7931490043fc4fb316a4aacbd25e
Daniel Mueller <deso@posteo.net>
2022-05-01 09:30:57 -0700

    Move slice_to_str into util module
    This change moves the existing slice_to_str function into the util
    module. This location makes it easier to reuse it in the future.

--- src/util.rs
+++ src/util.rs
@@ -5,6 +5,9 @@ use num_decimal::Num;
 
 use serde::Deserialize;
 use serde::Deserializer;
+use serde::Serialize;
+use serde::Serializer;
+use serde_variant::to_variant_name;
 
 
 /// Deserialize a `Num` from a string, parsing the value as signed first
@@ -25,4 +28,34 @@ where
 {
   let vec = Option::<Vec<T>>::deserialize(deserializer)?;
   Ok(vec.unwrap_or_else(Vec::new))
+}
+
+
+/// Serialize a slice into a string of textual representations of the
+/// elements separated by comma.
+///
+/// # Notes
+/// - this function should only be used for cases where `T` is an enum
+///   type
+pub(crate) fn slice_to_str<S, T>(slice: &[T], serializer: S) -> Result<S::Ok, S::Error>
+where
+  S: Serializer,
+  T: Serialize,
+{
+  if !slice.is_empty() {
+    // `serde_urlencoded` seemingly does not know how to handle a
+    // `Vec`. So what we do is we convert each and every element to a
+    // string and then concatenate them, separating each by comma.
+    let s = slice
+      .iter()
+      // We know that we are dealing with an enum variant and the
+      // function will never return an error for those, so it's fine
+      // to unwrap.
+      .map(|type_| to_variant_name(type_).unwrap())
+      .collect::<Vec<_>>()
+      .join(",");
+    serializer.serialize_str(&s)
+  } else {
+    serializer.serialize_none()
+  }
 }
