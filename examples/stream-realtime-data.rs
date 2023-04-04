22ec4a0eaea6588b4b3ff82b542eb57cb05238d3
Cemlyn <cemlynwaters@gmail.com>
2023-04-04 22:05:45 +0100

    Update example to use custom `Trade`
    This change updates the realtime data streaming example to use a custom
    trade type, illustrating how this can be done.

--- examples/stream-realtime-data.rs
+++ examples/stream-realtime-data.rs
@@ -4,16 +4,65 @@
 #![allow(clippy::let_unit_value)]
 
 use apca::data::v2::stream::drive;
+use apca::data::v2::stream::Bar;
 use apca::data::v2::stream::MarketData;
+use apca::data::v2::stream::Quote;
 use apca::data::v2::stream::RealtimeData;
 use apca::data::v2::stream::IEX;
 use apca::ApiInfo;
 use apca::Client;
 use apca::Error;
 
+use chrono::DateTime;
+use chrono::Utc;
+
 use futures::FutureExt as _;
 use futures::StreamExt as _;
 use futures::TryStreamExt as _;
+
+use num_decimal::Num;
+
+use serde::Deserialize;
+use serde::Serialize;
+
+
+/// A trade for an equity.
+///
+/// This is a user-defined type with additional fields compared to
+/// [`apca::data::v2::stream::Trade`] (the default), illustrating how to
+/// work with custom types.
+#[derive(Clone, Debug, Deserialize, Eq, PartialEq, Serialize)]
+pub struct Trade {
+  /// The trade's symbol.
+  #[serde(rename = "S")]
+  pub symbol: String,
+  /// The trade's ID.
+  #[serde(rename = "i")]
+  pub trade_id: u64,
+  /// The trade's price.
+  #[serde(rename = "p")]
+  pub trade_price: Num,
+  /// The trade's size.
+  #[serde(rename = "s")]
+  pub trade_size: u64,
+  /// The trade's conditions.
+  #[serde(rename = "c")]
+  pub conditions: Vec<String>,
+  /// The trade's time stamp.
+  #[serde(rename = "t")]
+  pub timestamp: DateTime<Utc>,
+  /// The trade's exchange.
+  #[serde(rename = "x")]
+  pub exchange: String,
+  /// The trade's tape.
+  #[serde(rename = "z")]
+  pub tape: String,
+  /// The trade's update, may be "canceled", "corrected", or
+  /// "incorrect".
+  #[serde(rename = "u", default)]
+  pub update: Option<String>,
+}
+
 
 #[tokio::main]
 async fn main() {
@@ -27,13 +76,18 @@ async fn main() {
   let api_info = ApiInfo::from_env().unwrap();
   let client = Client::new(api_info);
 
-  let (mut stream, mut subscription) = client.subscribe::<RealtimeData<IEX>>().await.unwrap();
+  let (mut stream, mut subscription) = client
+    .subscribe::<RealtimeData<IEX, Bar, Quote, Trade>>()
+    .await
+    .unwrap();
 
   let mut data = MarketData::default();
   // Subscribe to minute aggregate bars for SPY and XLK...
   data.set_bars(["SPY", "XLK"]);
-  // ... and realtime quotes for AAPL.
+  // ... and realtime quotes for AAPL...
   data.set_quotes(["AAPL"]);
+  // ... and realtime trades for TSLA.
+  data.set_trades(["TSLA"]);
 
   let subscribe = subscription.subscribe(&data).boxed();
   // Actually subscribe with the websocket server.
